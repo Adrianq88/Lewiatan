585e599a1ae1e846481e08322aec07e1686afba8
Alex4ndro5 <alek.folfas@gmail.com>
2023-10-04 17:37:11 +0200

    Syntax fixes

--- main.py
+++ main.py
@@ -9,11 +9,8 @@ from selenium.webdriver.common.proxy import Proxy, ProxyType
 import time
 
 
-PATH = r"C:\Users\ap9\AppData\Local\Microsoft\WindowsApps\chromedriver.exe"
-
 #initializing_NO_PROXY_driver
-driver = webdriver.Chrome(PATH)
-
+driver = webdriver.Chrome()
 #fullscreen
 driver.fullscreen_window()
 
@@ -43,10 +40,10 @@ time.sleep(0.5)
 #https://miniramp.pl/product-pol-29667-deska-Hockey-Kosovo-White.html
 
 
-#Dropdown menu with sizing (IN PROGRESS; UPDATE: shows sizes id)
-# dropdown = driver.find_element_by_id("projector_sizes_cont");
-# dropdown.click();
-# time.sleep(1);
+#Dropdown menu with sizing (IN PROGRESS UPDATE: shows sizes id)
+# dropdown = driver.find_element_by_id("projector_sizes_cont")
+# dropdown.click()
+# time.sleep(1)
 #
 # select = Select(driver.find_element_by_id('projector_sizes_select'))
 # select.select_by_visible_text("Wybierz rozmiar")
@@ -57,67 +54,66 @@ time.sleep(0.5)
 #
 # driver.quit()
 
-
 #checkout_process_with_accessibility_check_(in_progress)
-if (driver.find_element_by_id("oneclick_googlePay")):
+if (driver.find_element(By.ID, "oneclick_googlePay")):
 
     #checkout1
-    basket1 = driver.find_element_by_id("projector_button_basket");
-    basket1.click();
-    time.sleep(0.5);
+    basket1 = driver.find_element(By.ID, "projector_button_basket")
+    basket1.click()
+    time.sleep(0.5)
 
     #checkout2
-    basket2 = driver.find_element_by_id("basket_go_next");
-    basket2.click();
-    time.sleep(0.5);
+    basket2 = driver.find_element(By.ID, "basket_go_next")
+    basket2.click()
+    time.sleep(0.5)
 
     #checkout3
-    basket3 = driver.find_element_by_link_text("Zamów bez rejestracji");
-    basket3.click();
-    time.sleep(0.5);
+    basket3 = driver.find_element(By.LINK_TEXT, "Zamów bez rejestracji")
+    basket3.click()
+    time.sleep(0.5)
 
     #delivery rules
-    firstname = driver.find_element(By.ID, 'client_firstname');
-    lastname = driver.find_element(By.ID, 'client_lastname');
-    birth_day = driver.find_element(By.ID, 'birth_date');
-    street = driver.find_element(By.ID, 'client_street');
-    street_number = driver.find_element(By.ID, 'client_street_number');
-    zipcode = driver.find_element(By.ID, 'client_zipcode');
-    city = driver.find_element(By.ID, 'client_city');
-    phone = driver.find_element(By.ID, 'client_phone');
-    email = driver.find_element(By.ID, 'client_email');
+    firstname = driver.find_element(By.ID, 'client_firstname')
+    lastname = driver.find_element(By.ID, 'client_lastname')
+    birth_day = driver.find_element(By.ID, 'birth_date')
+    street = driver.find_element(By.ID, 'client_street')
+    street_number = driver.find_element(By.ID, 'client_street_number')
+    zipcode = driver.find_element(By.ID, 'client_zipcode')
+    city = driver.find_element(By.ID, 'client_city')
+    phone = driver.find_element(By.ID, 'client_phone')
+    email = driver.find_element(By.ID, 'client_email')
 
     #filling delivery
-    firstname.send_keys("Adrian"), time.sleep(0.2);
-    lastname.send_keys("Prędki"), time.sleep(0.2);
-    birth_day.send_keys("2002-08-06"), time.sleep(0.2);
-    street.send_keys("Zaciszna 8B"), time.sleep(0.2);
-    street_number.send_keys("8B"), time.sleep(0.2);
-    zipcode.send_keys("35-326"), time.sleep(0.2);
-    city.send_keys("Rzeszów"), time.sleep(0.2);
-    phone.send_keys("734467035"), time.sleep(0.2);
-    email.send_keys("ap9@op.pl"), time.sleep(0.2);
+    firstname.send_keys("Adrian"), time.sleep(0.2)
+    lastname.send_keys("Prędki"), time.sleep(0.2)
+    birth_day.send_keys("2002-08-06"), time.sleep(0.2)
+    street.send_keys("Zaciszna 8B"), time.sleep(0.2)
+    street_number.send_keys("8B"), time.sleep(0.2)
+    zipcode.send_keys("35-326"), time.sleep(0.2)
+    city.send_keys("Rzeszów"), time.sleep(0.2)
+    phone.send_keys("734467035"), time.sleep(0.2)
+    email.send_keys("ap9@op.pl"), time.sleep(0.2)
 
     #terms_agreement
-    terms_agree = driver.find_element_by_id("terms_agree");
-    terms_agree.click();
-    time.sleep(1);
+    terms_agree = driver.find_element(By.ID, "terms_agree")
+    terms_agree.click()
+    time.sleep(1)
 
     #sumbitting_form
-    submit_form = driver.find_element_by_id("submit_clientnew_form");
-    submit_form.click();
-    time.sleep(1);
+    submit_form = driver.find_element(By.ID, "submit_clientnew_form")
+    submit_form.click()
+    time.sleep(1)
 
     #payment_rules (in progress)
 
-    labels = driver.find_elements_by_css_selector("#content > form > div > div:nth-child(1) > section > div.order__payments_section")
+    labels = driver.find_element(By.CSS_SELECTOR, "#content > form > div > div:nth-child(1) > section > div.order__payments_section")
     for label in labels:
          print(label.text)
 
 
     # Lokalizuj wszystkie dostępne metody płatności (NIE DZIALA)
     #labels = driver.find_elements_by_css_selector("#content > form > div > div:nth-child(1) > section > div.order__payments_section")
-    #time.sleep(4);
+    #time.sleep(4)
     # Wybierz interesującą Cię metodę płatności, na przykład "BLIK"
     #desired_payment_method = "PayPo - kup teraz, zapłać za 30 dni"
 
@@ -132,8 +128,8 @@ if (driver.find_element_by_id("oneclick_googlePay")):
 
 
 
-elif (driver.find_elements_by_link_text("Powiadom mnie o dostępności produktu")):
-    print("Produkt niedostępny");
+elif (driver.find_element(By.LINK_TEXT, "Powiadom mnie o dostępności produktu")):
+    print("Produkt niedostępny")
 
 else:
     driver.quit()
